1df96895795664b7a962c2d560ddadb97d74809a
최원창 <vkdldjf0200@gmail.com>
2022-09-27 14:31:47 +0000

    TMC_block

--- Module/utils/tool.py
+++ Module/utils/tool.py
@@ -86,6 +86,79 @@ class Squeeze(nn.Module):
         return x.squeeze(-1).squeeze(-1)
     
     
+class TMC_Block(nn.Module):
+    
+    def __init__(self, in_channels, out_channels, cue_num, cut_point):
+        super(TMC_Block, self).__init__()
+        
+        self.make_block(in_channels, out_channels)
+        self.in_channels = in_channels
+        self.Cn = cue_num
+        
+    def forward(self, prev_inter_cue, prev_intra_cue):
+        
+        intra_cue_path = self.block[0]
+        TC_1_layer = self.block[1]
+        inter_cue_path = self.block[2]
+        
+        intra_C = []
+        inter_C = []
+        
+        """
+        Intra-Cue Path
+        
+        prev_intra_cue : (T X sum(in_channels))
+        intra_C : (T X out_channels)
+        """
+        
+        start_index = 0
+        for i in range(self.Cn):
+            x = prev_inter_cue[:, start_index:start_index + self.in_channels[i] ]	# cutting intra concatenated cue features
+            intra_C.append(intra_cue_path[i](x))
+            start_index = self.in_channels[i]
+        
+        intra_C = torch.cat(intra_C, dim=1)
+        intra_C = intra_cue_path[self.Cn](intra_C)
+        
+        """
+        Inter-Cue Path
+        
+        prev_inter_cue : (T X sum(in_channels))
+        intra_f : (T X out_channels)
+        """
+        
+        TC_1_output = TC_1_layer(intra_C)
+        
+        inter_C = torch.cat([inter_cue_path[0](prev_inter_cue), TC_1_output], dim=1)
+        
+        inter_C = inter_cue_path[1](inter_C)
+        
+        return inter_C, intra_C
+        
+        
+    def make_block(self, in_channels, out_channels):
+        
+        intra_cue_path = []
+        inter_cue_path = []
+        
+        for i in range(self.Cn):
+            intra_cue_path.append(nn.Conv1d(in_channels[i], int(out_channels/self.Cn), kernel_size = 5, padding = 2))
+        intra_cue_path.append(nn.ReLU(inplace=True))
+        intra_cue_path = nn.ModuleList(intra_cue_path)
+        
+        TC_1_layer = nn.Conv1d(sum(in_channels), int(out_channels/2), kernel_size=1)
+        
+        inter_cue_path = nn.ModuleList([nn.Conv1d(sum(in_channels), int(out_channels/2), kernel_size = 5, padding = 2), nn.ReLU(inplace=True)])
+        
+        self.block = nn.ModuleList([intra_cue_path, TC_1_layer, inter_cue_path])
+        
+        
+        
+        
+        
+        
+        
 def SMC_weight_reset(m):
     if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
         m.reset_parameters()
+        
